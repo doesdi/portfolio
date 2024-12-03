123222660b398be7a7144c98b9aa7c98e152f0d6
Evgeniy Samoilov <husokka@mail.ru>
2024-12-04 01:09:48 +0300

    feat(master): update html and, added js

--- portfolio/main/urls.py
+++ portfolio/main/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('', home, name = 'home'),
     path('about/', about, name = 'about')
+
 ]
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -80,8 +80,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-
-
     }
 }
 """
@@ -126,14 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
 STATIC_DIR = join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 
